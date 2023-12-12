fa704756d1febf49202a1aa0274695d9b282e8b6
KeyshaManucan <150794944+KeyshaManucan@users.noreply.github.com>
2023-12-12 23:54:35 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
-// JavaScript Document
-console.log("hi");
+let openButton = document.querySelector("header > button");
+let sluitButton = document.querySelector("nav button");
+
+openButton.onclick = openMenu;
+sluitButton.onclick = sluitMenu;
+
+function openMenu () {
+    let deNav = document.querySelector("nav")
+    deNav.classList.add("toonMenu");
+}
+
+function sluitMenu () {
+    let deNav = document.querySelector("nav");
+    deNav.classList.remove("toonMenu");
+}
+
